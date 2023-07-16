64e5e32454f73ab33c3473054ffe35979bfc7912
peytontan <peytontanhs@gmail.com>
2023-07-16 21:12:41 +0800

    edit and delete button now works

--- expense-tracker-client/src/components/TravelExpenses.jsx
+++ expense-tracker-client/src/components/TravelExpenses.jsx
@@ -15,6 +15,9 @@ export default function Travel() {
   const [selectedTrip, setSelectedTrip] = useState("");
   const [expenses, setExpenses] = useState([]);
 
+  // Introduce a new state variable for the selected expense
+  const [selectedExpense, setSelectedExpense] = useState(null);
+
   useEffect(() => {
     axios
       .get("http://localhost:3000/api/travel/displayTrips", {
@@ -38,13 +41,29 @@ export default function Travel() {
   };
 
   const handleFilterExpenses = (selectedTripName) => {
-    setSelectedTrip(selectedTripName);
+    setSelectedTrip(selectedTripName || "");
   };
 
   const handleFormChange = (e, fieldName) => {
     setFormData({
       ...formData,
       [fieldName]: e.target.value,
+    });
+  };
+
+
+  // Handler for selecting an expense for editing
+  const handleEdit = (expense) => {
+    setSelectedExpense(expense);
+    setFormData({
+      date: expense.date.substring(0, 10),
+      name: expense.name,
+      category: expense.category,
+      amount: expense.amount,
+      ccy: expense.ccy,
+      baseCCY: expense.baseCCY,
+      fx: expense.fx,
+      trip: expense.trip,
     });
   };
 
@@ -56,10 +75,94 @@ export default function Travel() {
       })
       .then((res) => {
         console.info(">>> create expense res: ", res);
-        window.location.reload();
+
+        // Fetch the updated list of trips
+        axios
+          .get("http://localhost:3000/api/travel/displayTrips", {
+            headers: {
+              Authorization: `Bearer ${Cookies.get("userAuthToken")}`,
+            },
+          })
+          .then((res) => {
+            console.info(">>> get trips res: ", res);
+            setTrips(res.data);
+          })
+          .catch((error) => {
+            console.error(">>> get trips error: ", error);
+          });
+
+        // Fetch the updated list of expenses
+        axios
+          .get("http://localhost:3000/api/travel/displayAll", {
+            headers: {
+              Authorization: `Bearer ${Cookies.get("userAuthToken")}`,
+            },
+          })
+          .then((res) => {
+            console.info(">>> get expense res: ", res);
+            setExpenses(res.data);
+          })
+          .catch((error) => {
+            console.error(">>> get expense error: ", error);
+          });
       })
       .catch((error) => {
         console.error(">>> create expense error: ", error);
+      });
+  };
+
+  const handleDelete = (expenseId) => {
+    axios
+      .post(`http://localhost:3000/api/travel/delete/${expenseId}`, formData, {
+        headers: { Authorization: `Bearer ${Cookies.get("userAuthToken")}` },
+      })
+      .then((res) => {
+        console.info(">>> delete expense res: ", res);
+
+        // Remove the deleted expense from the expenses state
+        setExpenses((prevExpenses) =>
+          prevExpenses.filter((exp) => exp._id !== expenseId)
+        );
+      })
+      .catch((error) => {
+        console.error(">>> delete expense error: ", error);
+      });
+  };
+
+  // Handler for selecting an expense for editing
+  const updateExpense = () => {
+    axios
+      .post(
+        `http://localhost:3000/api/travel/update/${selectedExpense._id}`,
+        formData,
+        {
+          headers: {
+            Authorization: `Bearer ${Cookies.get("userAuthToken")}`,
+          },
+        }
+      )
+      .then((res) => {
+        console.info(">>> update expense res: ", res);
+        setSelectedExpense(null);
+        setFormData({});
+  
+        // Fetch the updated list of expenses
+        axios
+          .get("http://localhost:3000/api/travel/displayAll", {
+            headers: {
+              Authorization: `Bearer ${Cookies.get("userAuthToken")}`,
+            },
+          })
+          .then((res) => {
+            console.info(">>> get expense res: ", res);
+            setExpenses(res.data);
+          })
+          .catch((error) => {
+            console.error(">>> get expense error: ", error);
+          });
+      })
+      .catch((error) => {
+        console.error(">>> update expense error: ", error);
       });
   };
 
@@ -283,8 +386,6 @@ export default function Travel() {
     { value: "ZWD", label: "ZWD" },
   ];
 
-  // Get user current monthly expenses
-  const [expenseNames, setExpenseNames] = useState(null);
 
   useEffect(() => {
     axios
@@ -293,14 +394,21 @@ export default function Travel() {
       })
       .then((res) => {
         console.info(">>> get expense res: ", res);
-        setExpenseNames(res.data);
+        
+        // Filter expenses based on the selected trip
+        const filteredExpenses = selectedTrip
+          ? res.data.filter((expense) => expense.trip === selectedTrip)
+          : res.data;
+        
+        setExpenses(filteredExpenses);
       })
       .catch((error) => {
         console.error(">>> get expense error: ", error);
       });
-  }, []);
+  }, [selectedTrip]);
+  
 
-  console.info(expenseNames);
+  console.info(expenses);
 
   return (
     <div>
@@ -308,7 +416,7 @@ export default function Travel() {
       <button onClick={logoutSuccess}>Logout</button>
 
       <form onSubmit={handleSubmit}>
-        <div>
+      <div>
           <label htmlFor="date">Date:</label>
           <input
             type="date"
@@ -395,6 +503,29 @@ export default function Travel() {
           </select>
         </div>
         <div>
+          <label htmlFor="baseCCY">Base Currency:</label>
+          <select
+            name="baseCCY"
+            id="baseCCY"
+            required
+            onChange={(e) => {
+              handleFormChange(e, "baseCCY");
+            }}
+          >
+            {/* TODO: selected throws warning, surpress? */}
+            <option value="placeholder" disabled={true} selected>
+              Select...
+            </option>
+            {currencyOptions.map(function (ccy, i) {
+              return (
+                <option value={ccy.value} key={i}>
+                  {ccy.label}
+                </option>
+              );
+            })}
+          </select>
+        </div>
+        <div>
           <label htmlFor="fx">Exchange Rate:</label>
           <input
             type="number"
@@ -452,34 +583,93 @@ export default function Travel() {
               <th>Name</th>
               <th>Category</th>
               <th>Amount</th>
+              <th>Edit</th>
+              <th>Delete</th>
             </tr>
           </thead>
           <tbody>
-            {expenseNames ? (
-              expenseNames
-                .filter((exp) => exp.trip === selectedTrip) // Filter expenses based on the selected trip
-                .map(function (exp, i) {
-                  return (
-                    <tr key={i}>
-                      <td>{exp.date.substring(0, 10)}</td>
-                      <td>{exp.name}</td>
-                      <td>{exp.category}</td>
-                      <td>{exp.amount}</td>
-                      <td>
-                        <button>Edit</button>
-                      </td>
-                      <td>
-                        <button>Delete</button>
-                      </td>
-                    </tr>
-                  );
-                })
-            ) : (
-              <></>
-            )}
+            {expenses.map((expense) => (
+              <tr key={expense._id}>
+                <td>{expense.date}</td>
+                <td>{expense.name}</td>
+                <td>{expense.category}</td>
+                <td>{expense.amount}</td>
+                <td>
+                  <button onClick={() => handleEdit(expense)}>Edit</button>
+                </td>
+                <td>
+                  <button onClick={() => handleDelete(expense._id)}>
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
           </tbody>
         </table>
       </div>
+
+      {/* Edit Expense Modal */}
+      {selectedExpense && (
+        <div>
+          <h2>Edit Expense</h2>
+          <form onSubmit={updateExpense}>
+            <div>
+              <label htmlFor="edit-date">Date:</label>
+              <input
+                type="date"
+                id="edit-date"
+                name="date"
+                value={formData.date || selectedExpense.date}
+                onChange={(e) => handleFormChange(e, "date")}
+              />
+            </div>
+            <div>
+              <label htmlFor="edit-name">Name:</label>
+              <input
+                type="text"
+                id="edit-name"
+                name="name"
+                value={formData.name || selectedExpense.name}
+                onChange={(e) => handleFormChange(e, "name")}
+              />
+            </div>
+            <div>
+              <label htmlFor="edit-category">Category:</label>
+              <select
+                id="edit-category"
+                name="category"
+                value={formData.category || selectedExpense.category}
+                onChange={(e) => handleFormChange(e, "category")}
+              >
+                {categoryOptions.map((option) => (
+                  <option value={option.value} key={option.value}>
+                    {option.label}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div>
+              <label htmlFor="edit-amount">Amount:</label>
+              <input
+                type="number"
+                id="edit-amount"
+                name="amount"
+                min="0"
+                step="0.01"
+                value={formData.amount || selectedExpense.amount}
+                onChange={(e) => {
+                  limitTwoDP(e);
+                  handleFormChange(e, "amount");
+                }}
+              />
+            </div>
+            <div>
+              <button type="submit">Save Changes</button>
+              <button onClick={() => setSelectedExpense(null)}>Cancel</button>
+            </div>
+          </form>
+        </div>
+      )}
     </div>
   );
 }
