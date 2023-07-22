6f99f11e607a929e8cbc7c5914c75474a305d30f
James Gay <jamesgayht@gmail.com>
2023-07-22 10:52:29 +0800

    testing button styles for deployment

--- expense-tracker-client/src/styles/GlobalStyle.js
+++ expense-tracker-client/src/styles/GlobalStyle.js
@@ -1,6 +1,6 @@
 import { createGlobalStyle } from "styled-components";
 
-export const GlobalStyle = createGlobalStyle`*
+export const GlobalStyle = createGlobalStyle`*  
 {
     margin: 0;
     padding: 0;
@@ -16,6 +16,23 @@ export const GlobalStyle = createGlobalStyle`*
     --color-grey: #aaa;
     --color-accent: #FD8A8A;
     --color-delete: #FF0000;
+
+    button {
+        font-family: 'Source Code Pro', sans-serif; 
+        font-size: clamp(1rem, 1.5vw, 1.2rem);
+        background: var(--color-accent);
+        padding: .3rem .8rem;
+        border-radius: 10px;
+        color: #;
+        box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
+        &:hover {
+            background: var(--color-blue);
+        }
+    }
+    
+    button:hover {
+        transform: scale(1.25);
+      }
 }
 
 body {
@@ -25,23 +42,6 @@ body {
     color: rgba(34, 34, 96, .6)';
 
 }
-
-button {
-    font-family: 'Source Code Pro', sans-serif; 
-    font-size: clamp(1rem, 1.5vw, 1.2rem);
-    background: var(--color-accent);
-    padding: .3rem .8rem;
-    border-radius: 10px;
-    color: #;
-    box-shadow: 0px 1px 15px rgba(0, 0, 0, 0.06);
-    &:hover {
-        background: var(--color-blue);
-    }
-}
-
-button:hover {
-    transform: scale(1.25);
-  }
 
 h1, h2, h3, h4, h5, h6 {
     color: var(--color-primary);
